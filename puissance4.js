b4e16f9efc6c0ab842453a52f61983a58153ee7e
dreamlumos <kiara.pbelle@gmail.com>
2019-10-01 12:33:30 +0200

    checkForWin except antislash diagonal

--- puissance4.js
+++ puissance4.js
@@ -6,7 +6,7 @@ for(var i = 0; i < board.length; i++) {
 
 }
 
-var player = "red";
+var player = "red"; // red starts the game
 
 function play(id) {
 
@@ -20,19 +20,111 @@ function play(id) {
 
 	if (row !== 6){
 
-			board[id][row] = player;
+			board[id][row] = player; // update board with new value
 			var element = document.getElementById(id+"."+row);
 			element.classList.remove("empty");
 			element.classList.add(player);
 
+			if (checkForWin(id, row)){
 
+				endGame();
 
+			} else {
+
+				switchplayer();	
+
+			}
 	}
-
-	switchplayer(player);
 
 }
 
+function checkForWin(id, row){
+
+	var rowPointer = 0;
+	var columnPointer = 0;
+	var counter = 0;
+
+	//check column
+
+	if (row >= 3) { //only check column if four or more rows are filled
+
+		for (rowPointer = 0; rowPointer<6; rowPointer++){
+
+			if (board[id][rowPointer] === player){
+				
+				counter++;
+
+				if (counter == 4){
+					return true;
+				}
+
+			} else {
+				
+				counter = 0;
+
+			}
+
+		}
+
+	}
+
+	//check row
+
+	counter = 0;
+
+	for (columnPointer = 0; columnPointer<7; columnPointer++){
+
+		if (board[columnPointer][row] === player){
+				
+				counter++;
+
+				if (counter == 4){
+					return true;
+				}
+
+			} else {
+				
+				counter = 0;
+
+			}
+
+	}
+
+	//check forward-slash diagonal
+
+	rowPointer = row;
+	columnPointer = id;
+	counter = 0;
+
+	while(board[columnPointer][rowPointer] === player && rowPointer>=0 && columnPointer>=0){ //left
+
+		columnPointer--;
+		rowPointer--;
+		counter++;
+
+		if (counter == 4){
+			return true;
+		}
+	}
+
+	rowPointer = row;
+	columnPointer = id;
+
+	while(board[columnPointer][rowPointer] === player && rowPointer<6 && columnPointer<7){ //right
+
+		columnPointer++;
+		rowPointer++;
+		counter++;
+
+		if (counter == 4){
+			return true;
+		}
+	}
+
+	//check anti-slash diagonal !!to finish
+
+
+}
 
 function switchplayer(){
 
@@ -46,7 +138,12 @@ function switchplayer(){
 
 	}
 
-	document.getElementById("player").innerHTML = "Joueur : "+toString(player);
+	document.getElementById("player").innerHTML = "Joueur : "+player;
 
 }
 
+function endGame(){
+
+	document.getElementById("player").innerHTML = "Joueur "+player+" a gagné !";
+
+}
